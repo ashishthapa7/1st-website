53a0b89e33668cdbbbdf4bda8c3e019d17a6e0cc
ashishthapa7 <ashishthapassr7@gmail.com>
2019-07-02 19:22:31 +0545

    changed totally

---
+++ java.js
@@ -0,0 +1,39 @@
+}
+.head .inner-head ul li {
+	display: inline;
+	text-decoration: none;
+	padding-top: 0px;
+	padding: 40px; 
+	margin: 30px;
+
+}
+.head .inner-head ul li:hover{
+	background-color: rgba(233,234,243,0.2);
+	color: rgb(211,211,211);
+	border-radius: 10px;
+}
+.main {
+	background-color: rgba(155,155,243);
+	text-align: center;
+	font-size: 80px;
+	height: 800px;
+	width: 100%;
+	margin: 0px;
+}
+/*.inner-main {
+	text-decoration: none;*/
+
+/*}*/
+/*.main .inner-main h1{
+	margin: 0px;
+	padding: 20px;
+	text-decoration: none;
+}
+.scroll {
+	background-color: rgba(195,175,36);
+	height: 200px;
+	width: 100%
+	margin: 0px;
+	
+}
+*/*/
